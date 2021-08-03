9c1113b34b40b03c1a38140b9b4f4b2a2f98e268
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-08-02 23:55:04 -0300

    Aula Interface e Polimorfismo prática

--- cap12_interface_polimorfismo_2.go
+++ cap12_interface_polimorfismo_2.go
@@ -1,5 +1,36 @@
 package main
 
+import "math"
+
+type geometria interface {
+	area() float64
+	perimetro() float64
+}
+
+type retangulo struct {
+	comprimento, altura float64
+}
+
+type circulo struct {
+	raio float64
+}
+
+type triangulo struct {
+	lado1, lado2, lado3 float64
+}
+
+func (r retangulo) area() float64 {
+	return r.comprimento * r.altura
+}
+
+func (r retangulo) perimetro() float64 {
+	return 2*r.comprimento + 2*r.altura
+}
+
+func (c circulo) area() float64 {
+	return math.Pi * c.raio * c.raio
+}
+
 func main() {
 
 }
