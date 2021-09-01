1b9059bb61dcd3b60cef70ca6d8b93777eb3611f
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-08-31 23:54:50 -0300

    Aula-exemplo canal unidirecional

--- exemplos/cap21/canaisUnidirecionais.go
+++ exemplos/cap21/canaisUnidirecionais.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	enviar := make(chan <- string)
+	canal := make(chan string)
+	go enviar(canal)
+	receber(canal)
+}
+
+func enviar(enviar chan <- string) {
+	fmt.Println("Enviando os dados...")
+	enviar <- "pacote de dados"
+}
+
+func receber(receber <- chan string) {
+	fmt.Println("Recebendo os dados ...", <- receber)
 }
