28ddeee48cd85199eddcdab328703f86f564a07e
Otávio Augusto Pinheiro <49002375+OtavioPinheiro@users.noreply.github.com>
2021-09-08 13:47:04 -0300

    Exercício02-cap24

---
+++ exercicios/cap24/exercicio02/main.go
@@ -0,0 +1,41 @@
+// Crie uma mensagem de erro customizada utilizando o fmt.Errorf()
+// Link: https://play.golang.org/p/9a1IAWy5E6
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type person struct {
+	First   string
+	Last    string
+	Sayings []string
+}
+
+func main() {
+	p1 := person{
+		First:   "James",
+		Last:    "Bond",
+		Sayings: []string{"Shaken, not stirred", "Any last wishes?", "Never say never"},
+	}
+
+	bs, err := toJSON(p1)
+	if err != nil {
+		log.Fatalln("Erro ao converter para JSON")
+	}
+
+	fmt.Println(string(bs))
+
+}
+
+// toJSON needs to return an error also
+func toJSON(a interface{}) ([]byte, error) {
+	bs, err := json.Marshal(a)
+	if err != nil {
+		return []byte{}, fmt.Errorf("Erro ao converter para JSON")
+	}
+	return bs, nil
+}
